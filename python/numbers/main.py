85683311398d3454164fdf8ed02d35a9f6a42797
tearoww <tinkkiski@gmail.com>
2021-09-02 13:46:32 +0300

    Initial commit of numbers

---
+++ python/numbers/main.py
@@ -0,0 +1,15 @@
+from math import *
+
+num = 6
+
+print(6 * 7)
+print(3 * (4 + 5))
+print(10 % 3)
+print(num)
+print(str(num) + " is my favorite number")
+print(abs(-5))
+print(pow(2, 8))
+print(max(1, 2, 3))
+print(round(min(5.5, 7.5, 12.5)))
+print(floor(5.9) + ceil(6.1))
+print(sqrt(256))

d8547b6eb33e7792f6fd75e09eca07bced76b93b
tearoww <tinkkiski@gmail.com>
2021-09-02 14:07:18 +0300

    Initial commit of madLibs

---
+++ python/madLibs/main.py
@@ -0,0 +1,10 @@
+print("Hi! Let's play Mad Libs. Give me a color:")
+color = input("> ")
+print("\nNice! Next, a plural noun:")
+noun = input("> ")
+print("\nAlright. Finally, a celebrity:")
+celebrity = input("> ")
+
+print("\nRoses are " + color)
+print(noun + " are blue")
+print("I love " + celebrity)

d0d678a2b66699f4f39ab419b4d81935f226acd2
tearoww <tinkkiski@gmail.com>
2021-09-02 09:57:50 +0300

    Initial commit of variables

---
+++ python/variables/main.py
@@ -0,0 +1,14 @@
+character_name = "Tom"
+character_age = "70"
+
+# character_age = 70
+# character_age = 50.5678213
+# is_male = True
+
+print("There once was a man named " + character_name + ".")
+print("He was " + character_age + " years old.")
+
+character_name = "Mike"
+
+print("He really liked the name " + character_name + ".")
+print("However, he didn't like being " + character_age + ".")

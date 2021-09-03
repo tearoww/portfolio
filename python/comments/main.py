b0dcf92609fa28d61671e179aed3709c6d4051e4
tearoww <tinkkiski@gmail.com>
2021-09-03 11:35:19 +0300

    Initial commit of comments

---
+++ python/comments/main.py
@@ -0,0 +1,9 @@
+# This is where I would write an explanation for what's coming next.
+# However, this time it's self-explanatory.
+
+"""
+This way I can have too much space to explain a program in the source code.
+Maybe I should use a readme at this point?
+"""
+
+print("Comments might help others understand my programs as well")

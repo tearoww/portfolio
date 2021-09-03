6c88fa6dbb19cb075cc896cd0ae7726792c81a6f
tearoww <tinkkiski@gmail.com>
2021-09-03 10:55:07 +0300

    Initial commit of forLoops

---
+++ python/forLoops/main.py
@@ -0,0 +1,13 @@
+buddies = ["Jesse", "Nikki", "Victor"]
+
+for character in "012345":
+    print(character)
+
+for buddy in buddies:
+    print(buddy)
+
+for index in range(6):
+    print(index)
+
+for index in range(len(buddies)):
+    print(buddies[index])

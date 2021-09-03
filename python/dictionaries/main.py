25dccece433c299d118dcea1844c7d7bce6a9e49
tearoww <tinkkiski@gmail.com>
2021-09-03 08:26:17 +0300

    Initial commit of dictionaries

---
+++ python/dictionaries/main.py
@@ -0,0 +1,17 @@
+months = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
+
+print(months["Dec"])
+print(months.get("Pyt", "Not a valid key!"))

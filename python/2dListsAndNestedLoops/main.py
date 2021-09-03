69288430ad8ef2219f5fde3966e1694094d3e7e7
tearoww <tinkkiski@gmail.com>
2021-09-03 11:10:29 +0300

    Initial commit of 2dListsAndNestedLoops

---
+++ python/2dListsAndNestedLoops/main.py
@@ -0,0 +1,17 @@
+grid = [
+    [0, 0],
+    [1, 1],
+    [2, 2],
+    [3, 3],
+    [4, 4],
+    [5, 5]
+]
+
+for index in grid:
+    print(index)
+
+print("done")
+
+for row in grid:
+    for column in grid:
+        print(column)

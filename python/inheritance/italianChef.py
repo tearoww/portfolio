f59f62ca107e347ec0ae43dd3364703e6c0d2358
tearoww <tinkkiski@gmail.com>
2021-09-07 09:54:08 +0300

    Initial commit of inheritance

---
+++ python/inheritance/italianChef.py
@@ -0,0 +1,10 @@
+from chef import Chef
+
+
+class ItalianChef(Chef):
+
+    def prepare_spaghetti_bolognese(self):
+        print("The chef prepares some spaghetti bolognese.")
+
+    def prepare_dessert(self):
+        print("The chef prepares a tiramisu")

dda47ef76b743334a0c05858fadab257364cedc3
tearoww <tinkkiski@gmail.com>
2022-05-24 17:55:50 +0300

    Removed hard coding

--- python/selenium/escScrape/main.py
+++ python/selenium/escScrape/main.py
@@ -1,23 +1,25 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+from datetime import datetime
 import openpyxl
 import pandas
 
 site = "https://eurovisionworld.com/eurovision/"
 driver = webdriver.Firefox()
+year = datetime.now().year
 all_tables = {}
-year = 2022
 
 driver.implicitly_wait(0.5)
 driver.maximize_window()
 
 while year >= 1956:
-    driver.get(site + str(year))
+    if year != 2020:
+        driver.get(site + str(year))
 
-    table_html = driver.find_element(By.CSS_SELECTOR, 'table.v_table:nth-child(1)')
-    table = pandas.read_html(table_html.get_attribute('outerHTML'))
+        table_html = driver.find_element(By.CSS_SELECTOR, 'table.v_table:nth-child(1)')
+        table = pandas.read_html(table_html.get_attribute('outerHTML'))
 
-    for df in table:
-        df.to_csv("/mnt/media/documents/csv/esc/" + str(year) + ".csv")
+        for df in table:
+            df.to_csv(str(year) + ".csv")
 
     year -= 1
